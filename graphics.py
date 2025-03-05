b248846ec729a957d1d4e76ff2a6f2539190fad6
tasso905 <tasso75@hotmail.com>
2025-03-04 22:19:53 -0500

    added a few classes

--- graphics.py
+++ graphics.py
@@ -18,6 +18,50 @@ class Window:
         while self.__running:
             self.redraw()
         print("window closed...")
+    
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color="black"):
+        self.fill_color = fill_color
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+
+        self.has_left_wall =  True
+        self.has_right_wall = True 
+        self.has_top_wall = True 
+        self.has_bottom_wall = True
+
+    def draw(self):
+        if self.has_left_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line, "black")
+        if self.has_right_wall:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line, "black")
+        if self.has_top_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(line, "black")
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from graphics import Window
+from graphics import Window, Line, Point, Cell
+import time
 
 def main():
     win = Window(800, 600)
+    #line = Line(Point(50,50), Point(400,400))
+    #win.draw_line(line, "black")
+    cell1 = Cell(50, 50, 100, 100, win)
+    cell1.has_left_wall = False
+    cell1.draw()
+
+    cell2 = Cell(125, 125, 200, 200, win)
+    cell2.has_right_wall = False
+    cell2.draw()
+
+    cell3 = Cell(225, 225, 250, 250, win)
+    cell3.has_bottom_wall = False
+    cell3.draw()
+
+    cell4 = Cell(300, 300, 500, 500, win)
+    cell4.has_top_wall = False
+    cell4.draw()
+
     win.wait_for_close()
 
 main()
